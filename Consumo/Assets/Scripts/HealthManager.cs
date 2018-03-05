fe901f23cf38dc669f64aadaae21c83b7ea23bec
Ja'ire <jv9ufxcy@yahoo.com>
2018-03-05 03:41:41 -0600

    Knockback Code is still not working as intended

---
+++ Consumo/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    public PlayerController playerController;
+	// Use this for initialization
+	void Start ()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    public void HurtPlayer(Vector3 direction)
+    {
+        playerController.Knockback(direction);
+    }
+}
---
+++ Consumo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    [SerializeField]
+    private float maxSpeed=10;
+
+    public float moveSpeed;
+    
+    private Rigidbody myRB;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    public float knockBackForce;
+    public float knockBackTime;
+    private float knockBackCounter;
+
+    // Use this for initialization
+    void Start () 
+	{
+        myRB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        PlayerMovement();
+	}
+
+    void PlayerMovement()
+    {
+        if (knockBackCounter<=0)
+        {
+            moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            moveVelocity = moveInput * moveSpeed;
+        }
+        else
+        {
+            knockBackCounter -= Time.deltaTime;
+        }
+        
+    }
+    void FixedUpdate()
+    {
+        float currentSpeed = Mathf.Abs(myRB.velocity.magnitude);
+        if (currentSpeed<maxSpeed)
+        {
+            myRB.AddForce(moveVelocity);
+        }
+    }
+
+    public void Knockback(Vector3 direction)
+    {
+        knockBackCounter = knockBackTime;
+
+        moveVelocity = direction * knockBackForce;
+    }
+}
